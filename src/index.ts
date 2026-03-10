a2565832de6aeb0a5f4403e870045894f6287f61
banamomo50-dot <banamomo50@gmail.com>
2026-03-10 12:34:42 +0700

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,9 @@
 import { R2Explorer } from "r2-explorer";
 
-export default R2Explorer({ 	readonly: false,
-  }
+export default R2Explorer({ readonly: false });
 
 	// Set to false to allow users to upload files
 
 	// Learn more how to secure your R2 Explorer instance:
 	// https://r2explorer.com/getting-started/security/
 	// cfAccessTeamName: "my-team-name",
-});
