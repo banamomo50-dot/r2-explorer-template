565f090e2395c82613afb565909aeb12b0ebf1e4
banamomo50-dot <banamomo50@gmail.com>
2026-03-10 12:33:35 +0700

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,8 @@
 import { R2Explorer } from "r2-explorer";
 
-export default R2Explorer({ readonly: false, basicAuth: {
-    username: 'admin',
-    password: 'esbatu533'
+export default R2Explorer({ 	readonly: false,
   }
-});
+
 	// Set to false to allow users to upload files
 
 	// Learn more how to secure your R2 Explorer instance:
